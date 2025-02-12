9d3ddd8c30a6bdaa87241aa74fccb45b690058d7
jpalvim <jalvim3@gatech.edu>
2025-02-12 15:23:48 -0600

    1st commit

---
+++ data_analysis/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SPARK_WORKLOAD=$1
+
+echo "SPARK_WORKLOAD: $SPARK_WORKLOAD"
+
+if [ "$SPARK_WORKLOAD" == "master" ];
+then
+  start-master.sh -p 7077
+elif [ "$SPARK_WORKLOAD" == "worker" ];
+then
+  start-worker.sh spark://spark-master:7077
+elif [ "$SPARK_WORKLOAD" == "history" ]
+then
+  start-history-server.sh
+elif [ "$SPARK_WORKLOAD" == "jupyter" ]
+then
+    jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token='' \
+    --notebook-dir='/opt/spark/notebooks'
+fi
